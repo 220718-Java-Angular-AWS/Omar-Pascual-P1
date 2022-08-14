0d8e4a109aab3723af2725c0d542b9093d7c3931
omar Pascual <pascual7@yahoo.com>
2022-08-14 16:41:29 -0400

    P1

--- P1/Documents/Reimburse_Schema.sql
+++ P1/Documents/Reimburse_Schema.sql
@@ -2,8 +2,7 @@ CREATE TABLE users (
 	user_id SERIAL,
 	username VARCHAR(200) NOT NULL UNIQUE,
 	email VARCHAR(200) NOT NULL,
-	"password" VARCHAR(200) NOT NULL,
-	CONSTRAINT users_pk PRIMARY KEY (user_id)
+	"password" VARCHAR(200) NOT NULL
 	);
 
 
@@ -13,6 +12,5 @@ CREATE TABLE reimburse (
 	ticket INT,
 	reason VARCHAR(2000) NOT NULL,
 	amount DECIMAL(1000, 2) NOT NULL,
-	pending BOOL,
-	CONSTRAINT reimburse_users_fk FOREIGN KEY (user_id) REFERENCES users (user_id)
+	pending BOOL
     );
